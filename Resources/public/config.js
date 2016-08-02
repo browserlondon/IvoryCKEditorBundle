12d1b929ca116de4e19f8e25c59caf2b81f80451
Josh Johnson <josh.johnson@browserlondon.com>
2016-08-02 16:33:09 +0100

    Import our css into ckeditor

--- Resources/public/config.js
+++ Resources/public/config.js
@@ -5,10 +5,14 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-    config.skin = 'twine-v2';
+	config.skin = 'twine-v2';
+    config.extraPlugins = 'autogrow';
+    config.autoGrow_minHeight = 250;
+    config.autoGrow_maxHeight = 600;
+    config.autoGrow_onStartup = true;
+    config.autoGrow_bottomSpace = 50;
     config.removePlugins = 'elementspath, resize';
     config.resize_enabled = false;
     config.forcePasteAsPlainText = true;
-	// config.uiColor = '#AADC6E';
+    config.contentsCss = '/../../styles/main.min.css';
 };
